df0090db394964b5f12574affe538dc349b94880
dimitriGallos <dimitrios.gallos@mail.mcgill.ca>
2013-10-06 00:19:02 -0400

    Turned the calculatePrimes method into a CalculatePrimes class
    This will improve readability since the different methods when lumped to one
    class had very different functions. Now one class responsible for each function
    of program.

---
+++ CalculatePrimes.java
@@ -0,0 +1,45 @@
+public class CalculatePrimes {
+	int numberOfPrimes;;
+	int[] listOfPrimes;
+	
+	public CalculatePrimes (int numberOfPrimes, int[] listOfPrimes ){
+		this.numberOfPrimes = numberOfPrimes;
+		this.listOfPrimes = listOfPrimes;
+	}
+	
+	public void calculatePrimes() {
+	      /* Two is the only even prime. All other prime numbers are odd.
+	       * To simplify the code, we simply add 2 as a prime number, and
+	       * delegate the task of finding all odd prime numbers to a helper
+	       * function. */
+		listOfPrimes[1] = 2;
+		calculateOddPrimes();
+	}
+
+		private void calculateOddPrimes() {
+			boolean isPrime;
+	        int primeListIndex;
+	      
+
+	        int currentNumber = 1;
+	        int indexOfSmallestPrimeFactor = 2;
+	        int squareOfPrimeFactor = 9;
+
+	        for(int primesFoundSoFar = 1; primesFoundSoFar < numberOfPrimes; primesFoundSoFar++) {
+	        	do {
+	            currentNumber = currentNumber + 2;
+	            if (currentNumber == squareOfPrimeFactor) {
+	            indexOfSmallestPrimeFactor = indexOfSmallestPrimeFactor + 1;
+	            squareOfPrimeFactor = listOfPrimes[indexOfSmallestPrimeFactor] * listOfPrimes[indexOfSmallestPrimeFactor];
+	            }
+	            isPrime = true;
+	            for (primeListIndex=1; primeListIndex < indexOfSmallestPrimeFactor && isPrime;primeListIndex++) {
+	            	if (currentNumber % listOfPrimes[primeListIndex] == 0) {
+	            		isPrime = false;
+	        	    }
+	            }
+	            } while (!isPrime);
+	            listOfPrimes[primesFoundSoFar + 1] = currentNumber;
+	        }
+	   }
+ }
--- PrintPrimes.java
+++ PrintPrimes.java
@@ -10,49 +10,12 @@ public class PrintPrimes {
     this.columnsPerPage  = columnsPerPage;
     this.listOfPrimes = new int[numberOfPrimes + 1];
   }
-
-
+  
   public static void main(String[] args) {
       PrintPrimes printPrimes = new PrintPrimes(300, 50, 4 );
-      printPrimes.calculatePrimes();
+      CalculatePrimes calculatePrimes= new CalculatePrimes(printPrimes.numberOfPrimes, printPrimes.listOfPrimes);
+      calculatePrimes.calculatePrimes();
       printPrimes.printPrimes();
-  }
-
-  public void calculatePrimes() {
-      /* Two is the only even prime. All other prime numbers are odd.
-       * To simplify the code, we simply add 2 as a prime number, and
-       * delegate the task of finding all odd prime numbers to a helper
-       * function.
-       */
-      listOfPrimes[1] = 2;
-      calculateOddPrimes();
-  }
-
-  private void calculateOddPrimes() {
-      boolean isPrime;
-      int primeListIndex;
-      
-
-      int currentNumber = 1;
-      int indexOfSmallestPrimeFactor = 2;
-      int squareOfPrimeFactor = 9;
-
-      for(int primesFoundSoFar = 1; primesFoundSoFar < numberOfPrimes; primesFoundSoFar++) {
-        do {
-          currentNumber = currentNumber + 2;
-          if (currentNumber == squareOfPrimeFactor) {
-            indexOfSmallestPrimeFactor = indexOfSmallestPrimeFactor + 1;
-            squareOfPrimeFactor = listOfPrimes[indexOfSmallestPrimeFactor] * listOfPrimes[indexOfSmallestPrimeFactor];
-          }
-          isPrime = true;
-          for (primeListIndex=1; primeListIndex < indexOfSmallestPrimeFactor && isPrime;primeListIndex++) {
-        	  if (currentNumber % listOfPrimes[primeListIndex] == 0) {
-                  isPrime = false;
-        	  }
-          }
-        } while (!isPrime);
-        listOfPrimes[primesFoundSoFar + 1] = currentNumber;
-      }
   }
 
     public void printPrimes() {
