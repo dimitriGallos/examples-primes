5ff859d66bc850d775c8ee61a44f6eeb243db25d
dimitriGallos <dimitrios.gallos@mail.mcgill.ca>
2013-10-06 00:35:30 -0400

    Fixed braces and spacings on printPrimes method

--- PrintPrimes.java
+++ PrintPrimes.java
@@ -13,23 +13,24 @@ public class PrintPrimes {
   
   public static void main(String[] args) {
       PrintPrimes printPrimes = new PrintPrimes(300, 50, 4 );
-      CalculatePrimes calculatePrimes= new CalculatePrimes(printPrimes.numberOfPrimes, printPrimes.listOfPrimes);
+      CalculatePrimes calculatePrimes = new CalculatePrimes(printPrimes.numberOfPrimes, printPrimes.listOfPrimes);
       calculatePrimes.calculatePrimes();
       printPrimes.printPrimes();
   }
 
     public void printPrimes() {
-        int pageNumber = 1;
+    	int pageNumber = 1;
         int pageOffset = 1;
         while (pageOffset <= numberOfPrimes) {
-          System.out.println("The First " + numberOfPrimes +
-                               " Prime Numbers --- Page " + pageNumber);
+          System.out.println("The First " + numberOfPrimes +" Prime Numbers --- Page " + pageNumber);
           System.out.println("");
           for (int rowOffset = pageOffset; rowOffset < pageOffset + primesPerColumn; rowOffset++){
-            for (int i = 0; i < columnsPerPage;i++)
-              if (rowOffset + i * primesPerColumn <= numberOfPrimes)
-                System.out.format("%10d", listOfPrimes[rowOffset + i * primesPerColumn]);
-            System.out.println("");
+        	  for (int i = 0; i < columnsPerPage;i++) {
+        		  if (rowOffset + i * primesPerColumn <= numberOfPrimes) {
+        			  System.out.format("%10d", listOfPrimes [ rowOffset + i * primesPerColumn ]);
+                  }
+               System.out.println("");
+              }
           }
           System.out.println("\f");
           pageNumber = pageNumber + 1;
